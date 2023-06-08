bcd9b0109d1268f6494f64a8e92ad7291a0a506f
ehoffman66 <erik.m.hoffman@gmail.com>
2023-06-07 22:59:12 -0400

    Update Music and Events page to have new styling and match the Portraits page

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,51 +1,108 @@
-// Music.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Masonry from 'react-masonry-css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './Gallery.css';
 import musicData from '../data/music.json';
 
 function Music() {
+  const navigate = useNavigate();
   const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
   const [imagePaths, setImagePaths] = useState([]);
 
-  useEffect(() => {
-    setImagePaths(musicData);
-  }, []);
+  const handleNext = useCallback(() => {
+    if (selectedImageIndex < imagePaths.length - 1) {
+      const newIndex = selectedImageIndex + 1;
+      setSelectedImage(process.env.PUBLIC_URL + imagePaths[newIndex].path);
+      setSelectedImageIndex(newIndex);
+    }
+  }, [selectedImageIndex, imagePaths]);
 
-  const handleClick = (path) => {
+  const handlePrev = useCallback(() => {
+    if (selectedImageIndex > 0) {
+      const newIndex = selectedImageIndex - 1;
+      setSelectedImage(process.env.PUBLIC_URL + imagePaths[newIndex].path);
+      setSelectedImageIndex(newIndex);
+    }
+  }, [selectedImageIndex, imagePaths]);
+
+  useEffect(() => {
+    navigate('/music');
+    setImagePaths(musicData);
+  }, [navigate]);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowLeft') {
+        handlePrev();
+      } else if (event.key === 'ArrowRight') {
+        handleNext();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleNext, handlePrev]);
+
+  const handleClick = (path, index) => {
     setSelectedImage(path);
+    setSelectedImageIndex(index);
   };
 
   const handleClose = () => {
     setSelectedImage(null);
+    setSelectedImageIndex(null);
   };
 
-// Code snippet from Portraits.js (same changes apply for Music.js and Events.js)
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
 
-return (
-    <div className="portraits-container">
-      <h1 className="header-text">Music</h1>
-      <div className="flex flex-wrap justify-start">
+  return (
+    <div className="gallery-container">
+      <h1 className="header-text">MUSIC</h1>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {imagePaths.map((item, index) => (
-          <div className="m-1 w-1/2 sm:w-1/3 lg:w-1/4" key={index}>
-            <img
-              className="w-full"
-              src={item.path}
-              alt={item.alt}
-              onClick={() => handleClick(item.path)}
-            />
+          <div
+            className="grid-item"
+            key={index}
+            onClick={() => handleClick(process.env.PUBLIC_URL + item.path, index)}
+          >
+            <img src={process.env.PUBLIC_URL + item.path} alt={item.alt} />
           </div>
         ))}
-      </div>
+      </Masonry>
       {selectedImage && (
         <div className="overlay" onClick={handleClose}>
           <div className="modal">
-            <img src={selectedImage} alt="Portrait" />
+            <span className="close" onClick={handleClose}><i className="fas fa-times"></i></span>
+            <img src={selectedImage} alt="Music" />
+            {selectedImageIndex > 0 && (
+              <button className="prev" onClick={handlePrev}>
+                <i className="fas fa-chevron-left"></i>
+              </button>
+            )}
+            {selectedImageIndex < imagePaths.length - 1 && (
+              <button className="next" onClick={handleNext}>
+                <i className="fas fa-chevron-right"></i>
+              </button>
+            )}
           </div>
         </div>
       )}
     </div>
   );
-  
 }
 
 export default Music;
