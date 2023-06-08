9823901e7d89247e9f79645afb47c88c55f724be
ehoffman66 <erik.m.hoffman@gmail.com>
2023-06-07 22:32:27 -0400

    Remove unneeded files and image and update Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../App.css'; // or '../MyComponent.css'
+import ContactForm from './ContactForm';
+import './Contact.css';
 
 function Contact() {
   return (
     <div>
       <h1 className="header-text">CONTACT</h1>
-      <p></p>
+      <div className="contact-form-container">
+        <ContactForm />
+      </div>
     </div>
   );
 }
