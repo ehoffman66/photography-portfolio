3bd591ee7b3fd30462a61d988a1ecb8428caeaae
ehoffman66 <erik.m.hoffman@gmail.com>
2023-08-11 22:52:00 -0400

    Update Navigation and app css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,39 +9,39 @@ function Navigation() {
     }
 
     return (
-      <nav className="navigation">
-        <div className="site-title text-6xl font-bold mb-4">
-          <div>Erik Hoffman</div>
-          <div>Photos</div>
-        </div>
-        <div onClick={toggleNav} className={`nav-toggler ${isOpen ? 'open' : 'closed'}`}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-        <ul className={`nav-menu ${isOpen ? 'nav-open' : ''}`}>
-          <li>
-            <Link to="/about" className="nav-link" activeClassName="active">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/portraits" className="nav-link" activeClassName="active">
-              PEOPLE
-            </Link>
-          </li>
-          <li>
-            <Link to="/music" className="nav-link" activeClassName="active">
-              Music
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="nav-link" activeClassName="active">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className="navigation">
+            <div className="site-title text-6xl font-bold mb-4">
+                <div className="title-line title-photos">ERIK HOFFMAN</div>
+                <div className="title-line">PHOTOS</div>
+            </div>
+            <div onClick={toggleNav} className={`nav-toggler ${isOpen ? 'open' : 'closed'}`}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            <ul className={`nav-menu ${isOpen ? 'nav-open' : ''}`}>
+                <li>
+                    <Link to="/about" className="nav-link" activeClassName="active">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/portraits" className="nav-link" activeClassName="active">
+                        PEOPLE
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/music" className="nav-link" activeClassName="active">
+                        Music
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="nav-link" activeClassName="active">
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
